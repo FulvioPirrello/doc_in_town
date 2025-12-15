873dab144c1053f8cd33e0a2cdb3e8f854ca53b0
Fulvio pirrello <fulviopirrello@Macbookinmammt.local>
2025-12-15 10:48:21 +0100

    v3

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
-use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ProfessionistaController;
 use App\Http\Controllers\PrenotazioniController;
 use App\Http\Controllers\UtenteController;
 
@@ -12,13 +12,13 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
-Route::get('/', [ItemController::class, 'professionisti'])->name('login'); 
+Route::get('/', [ProfessionistaController::class, 'professionisti'])->name('login'); 
 
-Route::get('/professionista/{slug}', [ItemController::class, 'show'])->name('mostra_professionista');
+Route::get('/professionista/{slug}', [ProfessionistaController::class, 'show'])->name('mostra_professionista');
 
 Route::post('/prenota/{id}', [PrenotazioniController::class, 'crea_prenotazione'])
     ->name('aggiungi_professionista');
-    
+
 Route::get('/profilo', [UtenteController::class, 'profilo_utente'])
     ->name('profilo_utente')
     ->middleware('auth');
